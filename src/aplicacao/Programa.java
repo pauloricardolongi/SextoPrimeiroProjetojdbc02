27f67e36b64a894c3f98b31c9b857dcf9e59c0dc
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-17 04:04:18 -0300

    classe Seller com teste principal

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,6 +1,9 @@
 package aplicacao;
 
+import java.util.Date;
+
 import model.entidades.Department;
+import model.entidades.Seller;
 
 public class Programa {
 
@@ -8,7 +11,8 @@ public class Programa {
 		
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(),3000.0 , obj);
+		System.out.println(seller);
 
 	}
 
