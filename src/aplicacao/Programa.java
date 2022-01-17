d444e09b6706848244b9ae2c5e68cf06b8e02fe8
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-17 02:04:23 -0300

    criado classe Departamento e teste classe principal

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import model.entidades.Department;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		
+		System.out.println(obj);
+
+	}
+
+}
