574d05f4468b3ac556c7cd9389d85b235ef3e36a
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-18 03:15:06 -0300

    buscar vendedor e departamento BD

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,18 +1,21 @@
 package aplicacao;
 
-import java.util.Date;
-
-import model.entidades.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entidades.Seller;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(),3000.0 , obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
+		
 
 	}
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.implementacao.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
