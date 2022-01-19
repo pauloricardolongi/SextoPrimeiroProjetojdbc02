478151e4bf8258ea4f6f7025f81f7a07362d67d7
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-19 04:17:55 -0300

    update nome

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -40,6 +40,14 @@ public class Programa {
 		Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserir! New id " + newSeller.getId());
+		
+		System.out.println("\n======TESTE 5: seller update========");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		
+		
+		sellerDao.update(seller);
+		System.out.println("Update completo");
 	}
 }
 
